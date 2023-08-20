f5bc7973e78984ab1baffba295d21f36cd8176f1
faridullahak <faridullah.akbari@tekschool.us>
2023-08-20 00:14:47 -0400

    last updates for mvn

---
+++ src/test/java/api/runner/RegressionRunner.java
@@ -0,0 +1,14 @@
+package api.runner;
+
+import com.intuit.karate.junit5.Karate;
+
+public class RegressionRunner {
+	@Karate.Test
+	public Karate runTest() {
+		//address for our feature files.
+		//And tags
+		return Karate.run("classpath:features")
+				.tags("@Regression");
+		
+	}
+}
--- src/test/java/api/runner/SmokeRunner.java
+++ src/test/java/api/runner/SmokeRunner.java
@@ -1,14 +1,14 @@
 package api.runner;
 import com.intuit.karate.junit5.Karate;
 
-public class TestRunner {
+public class SmokeRunner {
 
 	@Karate.Test
 	public Karate runTest() {
 		//address for our feature files.
 		//And tags
 		return Karate.run("classpath:features")
-				.tags("@Regression");
+				.tags("@Smoke");
 		
 	}
 }
